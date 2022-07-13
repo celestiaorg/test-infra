0b95658f71071422eb55fb5cf1ce243030ab7d08
Nhu Viet Nguyen <braveryandglory@gmail.com>
2022-07-13 13:46:58 +0200

    testground: add composition for the cluster env

--- testkit/sync.go
+++ testkit/sync.go
@@ -1,9 +1,13 @@
 package testkit
 
-import "github.com/testground/sdk-go/sync"
+import (
+	"github.com/celestiaorg/test-infra/testkit/appkit"
+	"github.com/testground/sdk-go/sync"
+)
 
 var (
 	AccountAddressTopic   = sync.NewTopic("account-address", "")
+	ValidatorPeerTopic    = sync.NewTopic("validator-info", &appkit.ValidatorNode{})
 	InitialGenenesisTopic = sync.NewTopic("initial-genesis", "")
 	GenesisTxTopic        = sync.NewTopic("genesis-tx", "")
 	BlockHashTopic        = sync.NewTopic("block-hash", "")
