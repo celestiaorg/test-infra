4400d3f7ece91e006b2e646b259ac584c26559f1
Nhu Viet Nguyen <braveryandglory@gmail.com>
2022-07-18 18:18:46 +0200

    fix: cleanup pfd signature

--- testkit/appkit/app.go
+++ testkit/appkit/app.go
@@ -105,7 +105,7 @@ func (ak *AppKit) StartNode(home string) error {
 	return svrcmd.Execute(ak.Cmd, appcmd.EnvPrefix, app.DefaultNodeHome)
 }
 
-func (ak *AppKit) PayForData(acc string, namespace []byte, msg []byte, krbackend, chainId, home string) (string, error) {
+func (ak *AppKit) PayForData(namespace []byte, msg []byte, krbackend, chainId, home string) (string, error) {
 	return ak.execCmd([]string{
 		"tx", "payment", "payForData", string(namespace), string(msg), "--node", "tcp://127.0.0.1:26657",
 		"--keyring-backend", krbackend, "--chain-id", chainId, "--home", home, "--keyring-dir", home,
