8977d7f028eb29cd8ad22baae6c585b908c07c5b
Nhu Viet Nguyen <braveryandglory@gmail.com>
2022-09-27 14:24:29 +0200

    feat: implementation of TC#005 Light nodes are dasing past headers faster than validators produce new ones
    - bumped versions of node containing new daser logs
    - bump go version in readme.md

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ var testcases = map[string]interface{}{
 	"001-val-large-txs":  run.InitializedTestCaseFn(tests.ValSubmitLargeTxs),
 	"002-da-sync":        run.InitializedTestCaseFn(tests.SyncNodes),
 	"003-full-sync-past": run.InitializedTestCaseFn(tests.FullSyncPast),
+	"005-light-das-past": run.InitializedTestCaseFn(tests.LightDasPast),
 }
 
 func main() {
