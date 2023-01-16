37769e68f7ba62129eadbf75651e9e1a0be92e73
Nhu Viet Nguyen <braveryandglory@gmail.com>
2023-01-16 11:03:39 +0100

    chore: apply changes to toml files. Remove debug info

--- testkit/appkit/app.go
+++ testkit/appkit/app.go
@@ -379,7 +379,6 @@ func AddSeedPeers(path string, peers []string) error {
 		peersStr.WriteString(fmt.Sprintf("%s:%d%s", peer, port, separator))
 	}
 
-	fmt.Println(peersStr.String())
 	return updateConfig(path, "p2p.seeds", peersStr.String())
 }
 
