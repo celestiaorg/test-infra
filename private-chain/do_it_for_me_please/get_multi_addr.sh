1aa0ebe4312dd96e24d092114246dc4e7889b2be
Jose Ramon Mañes <jose@celestia.org>
2023-06-16 14:55:07 +0200

    add: add trusted gen
    Signed-off-by: Jose Ramon Mañes <jose@celestia.org>

--- private-chain/do_it_for_me_please/get_multi_addr.sh
+++ private-chain/do_it_for_me_please/get_multi_addr.sh
@@ -4,6 +4,15 @@ clear
 
 LIGHT_FOLDER=light_nodes
 counter=0
+FILE=/tmp/trusted_peers.txt
+FILE_SHORT=/tmp/trusted_peers_short.txt
+
+if [ -f $FILE ];then
+    rm $FILE
+fi
+if [ -f $FILE_SHORT ];then
+    rm $FILE_SHORT
+fi
 
 # find the bridges and loop for each of them
 for i in $(kubectl -n bridge get po --sort-by=.metadata.name -oname);do
@@ -11,5 +20,10 @@ for i in $(kubectl -n bridge get po --sort-by=.metadata.name -oname);do
     ((counter++))
     kubectl -n bridge  logs $i |grep "The p2p host is listening on" -A2|grep "ip4"|tail -n+3|cut -d' ' -f3
     TRUSTED=$(kubectl -n bridge  logs $i |grep "The p2p host is listening on" -A4|grep "ip4"|grep "tcp"|cut -d' ' -f3)
+    TRUSTED_SHORT=$(echo $TRUSTED| cut -d'/' -f7)
     echo ${TRUSTED}
+    echo ${TRUSTED_SHORT}
+
+    echo ${TRUSTED} >> $FILE
+    echo ${TRUSTED_SHORT} >> $FILE_SHORT
 done
